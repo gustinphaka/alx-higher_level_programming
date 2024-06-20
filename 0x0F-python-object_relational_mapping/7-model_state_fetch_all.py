5dc0f24ed3411a819e9cedc46e82205fca735b6a
gustinphaka <gustinphaka@gmail.com>
2024-06-20 13:07:15 -0700

    updated objects

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,10 +8,11 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    for instance in session.query(State).order_by(State.id):
-        print(instance.id, instance.name, sep=": ")
+ engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+ .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+ Base.metadata.create_all(engine)
+ Session = sessionmaker(bind=engine)
+ session = Session()
+ for instance in session.query(State).order_by(State.id):
+ print(instance.id, instance.name, sep=": ")
+
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,13 +8,14 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    instance = session.query(State).first()
-    if instance is None:
-        print("Nothing")
-    else:
-        print(instance.id, instance.name, sep=": ")
+ engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+ .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+ Base.metadata.create_all(engine)
+ Session = sessionmaker(bind=engine)
+ session = Session()
+ instance = session.query(State).first()
+ if instance is None:
+ print("Nothing")
+ else:
+ print(instance.id, instance.name, sep=": ")
+
